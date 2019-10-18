4c91915d4b04d0a6282d90b101d142273d34c81f
Eric Tiano <etiano417@gmail.com>
2019-10-18 00:02:37 -0400

    Renamed pickled data to hundred_trial_data

--- histogram.py
+++ histogram.py
@@ -1,62 +1,9 @@
 from matplotlib import pyplot as plt
 import sys
 import datetime
-from gym import wrappers,logger
-import gym
 import argparse
 import random
 import pickle 
-
-
-
-class Agent(object):
-    """The world's simplest agent!"""
-    def __init__(self, action_space):
-        self.action_space = action_space
-
-    # You should modify this function
-    def act(self, observation, reward, done):
-        return self.action_space.sample()
-
-
-def runAgent(seed, Agent):
-    parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('--env_id', nargs='?', default='AsteroidsNoFrameskip-v4', help='Select the environment to run')
-    args = parser.parse_args()
-
-    # You can set the level to logger.DEBUG or logger.WARN if you
-    # want to change the amount of output.
-    logger.set_level(logger.INFO)
-
-    env = gym.make(args.env_id)
-
-    # You provide the directory to write to (can be an existing
-    # directory, including one with existing data -- all monitor files
-    # will be namespaced). You can also dump to a tempdir if you'd
-    # like: tempfile.mkdtemp().
-    outdir = 'random-agent-results'
-
-
-    env.seed(seed)
-    print(env.action_space)
-    agent = Agent(env.action_space)
-
-    episode_count = 100
-    reward = 0
-    done = False
-    score = 0
-    special_data = {}
-    special_data['ale.lives'] = 3
-    ob = env.reset()
-    while not done:
-        action = agent.act(ob, reward, done)
-        ob, reward, done, x = env.step(action)
-        #pdb.set_trace()
-        score += reward
-        env.render()
-
-
-    return score 
 
 
 def saveResultsToFile(results):
@@ -99,7 +46,7 @@ x = [1, 1, 2, 3 , 4, 4, 4, 5, 6, 10, 15, 15, 15, 20]
 #plt.show()
 
 #print(xTrials(100))
-results = unpickle("1571367060")
+results = unpickle("hundred_trial_data")
 results = results[0]
 resultsDict = results[1]
 results += [22250 for i in range (0, 21)]
